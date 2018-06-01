3bf4103ed7b59149c7d037208731426731df2b00
Tristan <tristancarter@gmail.com>
2018-06-01 13:11:23 -0400

    Added event to add value to local storage.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     $('.submitForm').on('click', function(){
         let text = $('.textField').val();
         $('.debug').text(text);
+        localStorage.setItem('myFormTextData', text);
     });
 
     $('.textField').on('keyup', function(){
