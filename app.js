c3511a879d963b57c64d66c0f098d7dee09c3ce5
Tristan <tristancarter@gmail.com>
2018-06-01 13:47:14 -0400

    Complete Implementation of Basic Reqs

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
 
-    $('.submitForm').on('click', function(){
+    $('.setData').on('click', function(){
         let text = $('.textField').val();
         $('.debug').text(text);
+        $('.textField').val('');
         localStorage.setItem('myFormTextData', text);
     });
 
     $('.textField').on('keyup', function(){
         let text = $('.textField').val();
         $('.debug').text(text);
+    });
+
+    $('.getData').on('click', function(){
+        let text = $('.textField').val();
+        let retrieve = localStorage.getItem('myFormTextData');
+        $('.textField').val('');
+        $('.debug').text(retrieve);
     });
 
 });
